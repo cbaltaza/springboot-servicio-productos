1758ff10367b408110f87aed2f690a6953a83c9f
cbaltaza <balta1_carlos@hotmail.com>
2023-05-22 18:52:04 -0500

    Cross Origin

--- src/main/java/com/banco/prueba/productos/web/controller/SaludoController.java
+++ src/main/java/com/banco/prueba/productos/web/controller/SaludoController.java
@@ -1,16 +1,14 @@
 package com.banco.prueba.productos.web.controller;
 
-import com.banco.prueba.productos.domain.model.Producto;
-import com.banco.prueba.productos.domain.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.banco.prueba.productos.domain.model.Producto;
+import com.banco.prueba.productos.domain.service.ProductoService;
 
 @RestController
 @RequestMapping("/saludar")
@@ -22,6 +20,8 @@ public class SaludoController {
     public String saludar() {
         return "Hola Mundo Carlos1";
     }
+    
+	@CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/{id}")
     public Producto getProduct(@PathVariable("id") int productId) {
         return productoService.recuperarProducto();
